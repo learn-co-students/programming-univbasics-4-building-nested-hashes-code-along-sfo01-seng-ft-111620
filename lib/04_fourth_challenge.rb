a7a141293155e250534c63d6f0881664408aafba
Lucas Montanari <lmonty2011@gmail.com>
2020-10-26 18:30:03 +0000

    Done.

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,8 +4,18 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [ 
         #your hashes here!
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
       ]
    }, 
    :capulet => {
@@ -14,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
       ]
    }
 }
